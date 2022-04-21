0957fe544de3ccb5a4b1ac71bf8812987bff9173
aphy18 <aphason1@gmail.com>
2022-04-20 23:56:12 -0400

    added event listener

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("GG WP")
+console.log("hello world")
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click',()=> {
+    console.log("You clicked me!!!")
+})
