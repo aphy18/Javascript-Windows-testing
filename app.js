4767da51868510098c40cbe723253d7503e8820e
aphy18 <aphason1@gmail.com>
2022-04-21 00:06:01 -0400

    developed add2numbers function

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const button = document.querySelector('#button');
 button.addEventListener('click',()=> {
     console.log("You clicked me!!!")
 })
+
+
+// simple function to add 2 numbers together
+const add2Numbers = (num1,num2) => {
+    if (typeof num1 !== 'number'|| typeof num2 !== 'number'){
+        console.log("unable to do addition")
+        return;
+    }
+
+    const sum = num1 + num2;
+    console.log("sum ---> ", sum)
+}
+
+add2Numbers("Hello my name is ", "Aphason")
+add2Numbers(2,2)
+add2Numbers(2,false)
