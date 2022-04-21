38d4f93d4f9bbd00ab2ff71c3037a695299645aa
aphy18 <aphason1@gmail.com>
2022-04-21 16:32:46 -0400

    background changes on click

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ console.log("GG WP")
 console.log("hello world")
 
 const button = document.querySelector('#button');
+const colorButton = document.querySelector('.color-button');
 
-button.addEventListener('click',()=> {
+
+
+
+
+
+
+
+button.addEventListener('click',() => {
     console.log("You clicked me!!!")
 })
+
+colorButton.addEventListener('click', () => {
+    let r = Math.floor(Math.random() * 255) + 1;
+    let g = Math.floor(Math.random() * 255) + 1;
+    let b = Math.floor(Math.random() * 255) + 1;
+    let randColor = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = randColor;
+    console.log('r --> ', r)
+})
+
+
+
+
 
 
 // simple function to add 2 numbers together
